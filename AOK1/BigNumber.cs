ac69643839a9498705fb31d5ef577eefd4456e43
Владислав Валерійович <vladyslavsyrotyuk@gmail.com>
2021-12-17 04:35:25 +0200

    / added

--- AOK1/BigNumber.cs
+++ AOK1/BigNumber.cs
@@ -7,6 +7,8 @@ namespace AOK1
     {
         private const int _lenght = 4; //length of number piece
 
+        private const int _base = 10000;
+
         private string _numberPiece = "";
 
         public List<int> Number { get; private set; } = new List<int>();
@@ -16,9 +18,16 @@ namespace AOK1
         public string UnsignedRawNum { get; private set; } = "";
         public string RawNum { get; private set; } = "";
 
-
+        #region Lab1
         public BigNumber(string number)
         {
+            number = number.TrimStart('0');
+
+            if (String.IsNullOrEmpty(number))
+            {
+                number = "0";
+            }
+
             RawNum = number;
 
             if (char.Equals(number[0], '-'))
@@ -53,16 +62,17 @@ namespace AOK1
                     }
                 }
             }
+
+            if (this.IsZero())
+            {
+                Number.Clear();
+                Number.Add(0);
+            }
         }
 
-        public string GetNumber()
+        public string GetUnsignedNumber()
         {
             var number = "";
-
-            if (NegativeSign == true)
-            {
-                number += "-";
-            }
 
             for (int i = Number.Count - 1; i >= 0; i--)
             {
@@ -87,9 +97,39 @@ namespace AOK1
             return number;
         }
 
+        public string GetNumber()
+        {
+            var number = "";
+
+            if (NegativeSign == true)
+            {
+                number += "-";
+            }
+
+            number += this.GetUnsignedNumber();
+
+            return number;
+        }
+
         public List<int> GetNumberByPieces()
         {
             return Number;
+        }
+
+        public bool IsZero()
+        {
+            for (int i = 0; i < Number.Count; i++)
+            {
+                if (Number[i] == 0)
+                {
+                    continue;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
         }
 
         public static BigNumber operator +(BigNumber number)
@@ -101,16 +141,25 @@ namespace AOK1
         {
             var countBig = 0;
             var countSmall = 0;
-            BigNumber result = new BigNumber(a.RawNum);
-            BigNumber other = new BigNumber(b.RawNum);
+            BigNumber result = new BigNumber(a.GetNumber());
+            BigNumber other = new BigNumber(b.GetNumber());
+
+            if (result.NegativeSign == false && other.NegativeSign == true)
+            {
+                return (new BigNumber(result.RawNum) - new BigNumber(other.UnsignedRawNum));
+            } // a > 0, b < 0 -> a - b
+            if (result.NegativeSign == true && other.NegativeSign == false)
+            {
+                return (new BigNumber(other.RawNum) - new BigNumber(result.UnsignedRawNum));
+            } // a < 0, b > 0 -> b - a
 
             if (a.Number.Count < b.Number.Count)
             {
                 countBig = b.Number.Count;
                 countSmall = a.Number.Count;
 
-                result = new BigNumber(b.RawNum);
-                other = new BigNumber(a.RawNum);
+                result = new BigNumber(b.GetNumber());
+                other = new BigNumber(a.GetNumber());
             }
             else
             {
@@ -118,13 +167,22 @@ namespace AOK1
                 countSmall = b.Number.Count;
             }
 
-            var piece = "";
-            var pieceForAdd = "";
+            if (a.IsZero())
+            {
+                return b;
+            }
+            else if (b.IsZero())
+            {
+                return a;
+            }
+
+            var piece = 0;
+            var pieceForAdd = 0;
 
             for (int i = 0; i < countBig; i++)
             {
-                piece = "0";
-                pieceForAdd = "0";
+                piece = 0;
+                pieceForAdd = 0;
 
                 for (int j = i; j < countSmall; j++)
                 {
@@ -132,27 +190,45 @@ namespace AOK1
                     break;
                 }
 
-                if (Convert.ToString(result.Number[i]).Length > 4)
+                if (result.Number[i] >= _base)
                 {
-                    piece = Convert.ToString(result.Number[i]);
-                    pieceForAdd = piece.Substring(0, piece.Length - 4);
-                    piece = piece.Substring(piece.Length - 4);
+                    piece = result.Number[i] % _base;
+                    pieceForAdd = (int)(result.Number[i] / _base);
 
-                    result.Number[i] = int.Parse(piece);
+                    result.Number[i] = piece;
 
                     if (i < countSmall - 1)
                     {
-                        result.Number[i + 1] += int.Parse(pieceForAdd);
+                        result.Number[i + 1] += pieceForAdd;
                     }
                     else
                     {
-                        result.Number.Add(int.Parse(pieceForAdd));
+                        result.Number.Add(pieceForAdd);
                     }
                 }
+                #region 
+                //if (Convert.ToString(result.Number[i]).Length > 4)
+                //{
+                //    piece = Convert.ToString(result.Number[i]);
+                //    pieceForAdd = piece.Substring(0, piece.Length - 4);
+                //    piece = piece.Substring(piece.Length - 4);
+
+                //    result.Number[i] = int.Parse(piece);
+
+                //    if (i < countSmall - 1)
+                //    {
+                //        result.Number[i + 1] += int.Parse(pieceForAdd);
+                //    }
+                //    else
+                //    {
+                //        result.Number.Add(int.Parse(pieceForAdd));
+                //    }
+                //}
+                #endregion
             }
 
             return result;
-        } // only for numbers > 0
+        }
 
         public static BigNumber operator -(BigNumber number)
         {
@@ -164,8 +240,8 @@ namespace AOK1
         {
             var countRes = a.Number.Count;
             var countOth = b.Number.Count;
-            BigNumber result = new BigNumber(a.RawNum);
-            BigNumber other = new BigNumber(b.RawNum);
+            BigNumber result = new BigNumber(a.GetNumber());
+            BigNumber other = new BigNumber(b.GetNumber());
 
             if (result == other)
             {
@@ -175,7 +251,7 @@ namespace AOK1
                 }
                 if (result.NegativeSign == true)
                 {
-                    return -(new BigNumber(result.UnsignedRawNum) + new BigNumber(other.UnsignedRawNum));
+                    return -(new BigNumber(result.GetUnsignedNumber()) + new BigNumber(other.GetUnsignedNumber()));
                 }
             } // a == b -> if sign = + then 0 | if sign = - then - (a + b)
 
@@ -186,12 +262,12 @@ namespace AOK1
 
             if (result.NegativeSign == true && other.NegativeSign == false)
             {
-                return -(new BigNumber(result.UnsignedRawNum) + new BigNumber(other.UnsignedRawNum));
+                return -(new BigNumber(result.GetUnsignedNumber()) + new BigNumber(other.GetUnsignedNumber()));
             } // a < 0, b > 0 -> -(a + b) where a,b > 0
 
             if (result.NegativeSign == true && other.NegativeSign == true)
             {
-                return -(new BigNumber(result.UnsignedRawNum) - new BigNumber(other.UnsignedRawNum));
+                return -(new BigNumber(result.GetUnsignedNumber()) - new BigNumber(other.GetUnsignedNumber()));
             } // a < 0, b < 0 -> -(+a - +b) where  a,b > 0    ??????????
 
             if (result.NegativeSign == false && other.NegativeSign == false)
@@ -216,7 +292,7 @@ namespace AOK1
                 } // HERE a > b
                 if (result < other)
                 {
-                    return -(new BigNumber(other.UnsignedRawNum) - new BigNumber(result.UnsignedRawNum));
+                    return -(new BigNumber(other.GetUnsignedNumber()) - new BigNumber(result.GetUnsignedNumber()));
                 } // a < b then -(b - a)
             }// EVERYTHING COMES HERE(or to +) a > 0, b > 0 -> a - b
 
@@ -234,31 +310,41 @@ namespace AOK1
                 return false;
             }
 
-            if (a.UnsignedRawNum.Length < b.UnsignedRawNum.Length)
+            if (a.Number.Count > b.Number.Count)
             {
-                return (a.NegativeSign == true && b.NegativeSign == true) ? false : true;
-                //return false;
+                return false;
             }
-            else if (a.UnsignedRawNum.Length > b.UnsignedRawNum.Length)
-            {
-                return (a.NegativeSign == true && b.NegativeSign == true) ? true : false;
-                //return true;
-            }
-
-            if (a.Number.Count < b.Number.Count)
+            else if (a.Number.Count < b.Number.Count)
             {
                 return true;
             }
 
+
             for (int i = b.Number.Count - 1; i >= 0; i--)
+            //for (int i = 0; i < b.Number.Count; i++)
             {
                 if (a.Number[i] < b.Number[i])
                 {
                     return true;
                 }
+                else if (a.Number[i] == b.Number[i]) continue;
+                else
+                {
+                    return false;
+                }
             }
 
             return false;
+        }
+
+        public static bool operator <=(BigNumber a, BigNumber b)
+        {
+            return (a < b || a == b) ? true : false;
+        }
+
+        public static bool operator >=(BigNumber a, BigNumber b)
+        {
+            return (a > b || a == b) ? true : false;
         }
 
         public static bool operator >(BigNumber a, BigNumber b)
@@ -292,5 +378,166 @@ namespace AOK1
         {
             return ((a < b) == false && (a > b) == false) ? false : true;
         }
+        #endregion
+        #region Lab2
+        public static BigNumber operator *(BigNumber a, BigNumber b)
+        {
+            if (a.IsZero() || b.IsZero())
+            {
+                return new BigNumber("0");
+            }
+
+            var bigNumList = new List<BigNumber>();
+            var resString = "";
+            var temp = 0;
+
+            for (int i = 0; i < a.Number.Count; i++)
+            {
+                var resList = new List<int>();
+                resString = "";
+                temp = 0;
+
+                for (int j = 0; j < i; j++)
+                {
+                    resList.Add(0);
+                }
+
+                for (int j = 0; j < b.Number.Count; j++)
+                {
+                    var bigTemp = (a.Number[i] * b.Number[j]) + temp;
+                    if (bigTemp > _base)
+                    {
+                        temp = (int)(bigTemp / _base);
+                    }
+                    else
+                    {
+                        temp = 0;
+                    }
+                    resList.Add(bigTemp % _base);
+                }
+                resList.Add(temp);
+
+                for (int f = resList.Count - 1; f >= 0; f--)
+                {
+                    var s = resList[f].ToString();
+                    if (resList[f] == 0)
+                    {
+                        resString += "0000";
+                    }
+                    else
+                    {
+                        if (s.Length < 4)
+                        {
+                            for (int j = s.Length; j < 4; j++)
+                            {
+                                resString += "0";
+                            }
+                        }
+                        resString += Convert.ToString(resList[f]);
+                    }
+                }
+
+                bigNumList.Add(new BigNumber(resString));
+            }
+
+            var tmp = bigNumList[0];
+            var result = bigNumList[0];
+
+            for (int i = 1; i < bigNumList.Count; i++)
+            {
+                result = tmp + bigNumList[i];
+                tmp = result;
+            }
+
+            return result;
+        }
+
+        public static BigNumber FastMultip(BigNumber a, BigNumber b)
+        {
+            if (a.IsZero() || b.IsZero())
+            {
+                return new BigNumber("0");
+            }
+
+            var S = new BigNumber("0");
+
+            while (b > new BigNumber("0"))
+            {
+                if (b.Number[0] % 2 == 0)
+                {
+                    a += a;
+                    b /= new BigNumber("2");
+                    Console.WriteLine("Other Processing");
+                }
+                else
+                {
+                    S += a;
+                    b -= new BigNumber("1");
+                    Console.WriteLine("Processing");
+                }
+            }
+
+            return S;
+        }
+
+        //public static BigNumber 
+
+        #endregion
+        #region Lab3
+        public static BigNumber operator /(BigNumber a, BigNumber b)
+        {
+            if (a == b)
+            {
+                return new BigNumber("1");
+            }
+            if (a < b)
+            {
+                return new BigNumber("0");
+            }
+
+            ulong min = 0, C = 10;
+            ulong num = _base;
+
+            for (ulong i = 1; !((new BigNumber(i.ToString()) * b) > a); i *= _base)
+            {
+                min = i;
+            }
+
+            while (true)
+            {
+                C = min + num;
+                var Multed = new BigNumber(C.ToString()) * b;
+
+                if (Multed == a )
+                {
+                    return new BigNumber(C.ToString());
+                }
+
+                if (Multed < a)
+                {
+                    min += num;
+                }
+                else
+                {
+                    if (num % 2 == 0)
+                    {
+                        num /= 2;
+                    }
+                    else
+                    {
+                        num = (num - 1) / 2;
+                    }
+                };
+                if (num == 0) break;
+            }
+
+            return new BigNumber(C.ToString());
+        }
+
+        public static BigNumber operator %(BigNumber a, BigNumber b)
+        {
+            return a - (b *(a / b));
+        }
+        #endregion
     }
 }
--- AOK1/Program.cs
+++ AOK1/Program.cs
@@ -6,30 +6,75 @@ namespace AOK1
     {
         static void Main(string[] args)
         {
-            var num = "9999999";
-            var num2 = "-13490561";
-            //var num2 = "88888";
+            var num = "1231231212323448883";//"123123121231233";//"999999289253245325452344532453425927653274865897327659";
+            var num2 = "943128121221";//"34543229156691224532523532480465873416952";
 
             var bigNum = new BigNumber(num);
             var bigNum2 = new BigNumber(num2);
 
-            var bigNum3 = bigNum - bigNum2;
+            var bigNum3 = bigNum + bigNum2;
+            var bigNum4 = bigNum - bigNum2;
+            var bigNum5 = bigNum * bigNum2;
+            var bigNum6 = bigNum / bigNum2;
+            var bigNum7 = bigNum % bigNum2;
+            //var bigNum8 = BigNumber.FastMultip(bigNum, bigNum2);
 
-            Console.WriteLine(bigNum.GetNumber());
-            Console.WriteLine(bigNum2.GetNumber());
-            Console.WriteLine();
-            Console.WriteLine(bigNum3.GetNumber());
-            Console.WriteLine();
-            Console.WriteLine(bigNum == bigNum2);
-            Console.WriteLine(bigNum > bigNum2);
-            Console.WriteLine(bigNum < bigNum2);
-            //TODO + if a or b< 0; zeros
+            Console.WriteLine($"First number = {bigNum.GetNumber()}");
+            Console.WriteLine($"Second number = {bigNum2.GetNumber()}\n");
 
-            foreach (var i in bigNum3.Number)
-            {
-                Console.Write(i);
-                Console.Write(" ");
-            }
+            Console.WriteLine($"Adding two numbers = {bigNum3.GetNumber()}");
+            Console.WriteLine($"Subtraction of two numbers = {bigNum4.GetNumber()}");
+            Console.WriteLine($"Multiplication of two numbers = {bigNum5.GetNumber()}");
+           // Console.WriteLine($"Fast Multiplication of two numbers = {bigNum8.GetNumber()}\n");
+
+            Console.WriteLine($"Dividing of two numbers = {bigNum6.GetNumber()}");
+            Console.WriteLine($"Mode of two numbers = {bigNum7.GetNumber()}\n");
+
+            Console.WriteLine($"Are two numbers equal = {bigNum == bigNum2}");
+            Console.WriteLine($"Is First greater = {bigNum > bigNum2}");
+            Console.WriteLine($"Is Second greater = {bigNum < bigNum2}\n");
+
+            Console.WriteLine($"Is First number zero = {bigNum.IsZero()}");
+            Console.WriteLine($"Is Second number zero = {bigNum2.IsZero()}\n");
+
+            //Console.Write("Numbers:\n");
+
+            //foreach (var i in bigNum.Number)
+            //{
+            //    Console.Write(i);
+            //    Console.Write(" ");
+            //}
+
+            //Console.WriteLine("\n");
+
+            //foreach (var i in bigNum2.Number)
+            //{
+            //    Console.Write(i);
+            //    Console.Write(" ");
+            //}
+            //Console.WriteLine("\n");
+
+
+            //foreach (var i in bigNum3.Number)
+            //{
+            //    Console.Write(i);
+            //    Console.Write(" ");
+            //}
+            //Console.WriteLine("\n");
+
+            //foreach (var i in bigNum4.Number)
+            //{
+            //    Console.Write(i);
+            //    Console.Write(" ");
+            //}
+            //Console.WriteLine("\n");
+
+            //foreach (var i in bigNum5.Number)
+            //{
+            //    Console.Write(i);
+            //    Console.Write(" ");
+            //}
+            //Console.WriteLine("\n");
         }
     }
 }
